930806776893691c91eb01993e793cc1b7e65a70
adrian <adrian657984@gmail.com>
2025-06-10 03:20:36 +0800

    using azure openai correctly

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from dotenv import load_dotenv
 import os
-from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain.chat_models import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI
 
 load_dotenv("sample.env")
 
 llm = AzureChatOpenAI(
-    deployment_name=os.getenv("Microsoft.CognitiveServicesOpenAI-20250610015116"),
-    openai_api_version="2024-05-01",
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_key=os.getenv("AZURE_OPENAI_KEY"),
+    azure_deployment=os.getenv("AZURE_DEPLOYMENT_NAME"),
+    api_version="2025-01-01-preview",
 )
 
 resp = llm.invoke("What are you?")
