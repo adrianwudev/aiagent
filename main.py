9fe984e09dd18b90f2d68c5e91a7f45ca6d3a110
AdrianWu <Adrian.Wu@aveva.com>
2025-06-10 16:46:18 +0800

    Implement simple agent and memory

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
 from dotenv import load_dotenv
 import os
+from pydantic import BaseModel
 from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool
+from memory.PersistentFileMemory import PersistentFileMemory
+
 
 load_dotenv("sample.env")
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 
 llm = AzureChatOpenAI(
     azure_deployment=os.getenv("AZURE_DEPLOYMENT_NAME"),
     api_version="2025-01-01-preview",
 )
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
-resp = llm.invoke("What are you?")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
 
-print(resp)
+tools = [search_tool, wiki_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools,
+)
+
+memory = PersistentFileMemory(
+    memory_key="chat_history",
+    return_messages=True,
+    file_path="chat_memory.json"
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True)
+query = input("Enter your query: ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output"))
+    print("Prompt Successfully Parsed.")
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
---
+++ memory/PersistentFileMemory.py
@@ -0,0 +1,25 @@
+import json
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import messages_from_dict, messages_to_dict
+from typing import Optional
+
+class PersistentFileMemory(ConversationBufferMemory):
+    file_path: Optional[str] = "memory.json"  # Declare to an property with a default value
+
+    def __init__(self, file_path="memory.json", **kwargs):
+        super().__init__(**kwargs)
+        object.__setattr__(self, "file_path", file_path)  # Use object.__setattr__ for pydantic
+        self._load_memory()
+
+    def _load_memory(self):
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                messages_dict = json.load(f)
+                self.chat_memory.messages = messages_from_dict(messages_dict)
+        except FileNotFoundError:
+            self.chat_memory.messages = []
+
+    def save_context(self, inputs, outputs):
+        super().save_context(inputs, outputs)
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump(messages_to_dict(self.chat_memory.messages), f, indent=2)
